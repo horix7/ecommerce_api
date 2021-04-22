81533e4f5c3ff33c6748af7a089921767a03812e
horix7 <horix77777@gmail.com>
2021-04-22 10:31:46 +0200

    adding caching to controllers

---
+++ src/middlewares/cachingresults.js
@@ -0,0 +1,26 @@
+import cache from 'memory-cache'
+
+const memCache = new cache.Cache()
+
+class Caching {
+
+     
+    cacheMiddleware = (req, res, next) => {
+
+            let key =  '__express__' + req.originalUrl || req.url
+            let cacheContent = memCache.get(key);
+            if(cacheContent){
+                res.send( cacheContent );
+                return
+            }else{
+                res.sendResponse = res.json
+                res.json = (body) => {
+                    memCache.put(key,body, 1200 * 1000);
+                    res.sendResponse(body)
+                }
+                next()
+            }
+    }
+}
+
+export default new Caching
